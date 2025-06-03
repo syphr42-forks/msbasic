2466dcb121c07ab8670c81aee8368fe15d1c3762
Gregory Moyer <greg@commitpush.run>
2025-06-03 12:44:20 -0400

    Fix zeropage and remove CRLF overrides

--- defines_eater.s
+++ defines_eater.s
@@ -15,10 +15,10 @@ CONFIG_2A := 1
 CONFIG_SCRTCH_ORDER := 2
 
 ; zero page
-ZP_START1 = $00
-ZP_START2 = $0A
-ZP_START3 = $60
-ZP_START4 = $6B
+ZP_START1 = $02
+ZP_START2 = $0C
+ZP_START3 = $62
+ZP_START4 = $6D
 
 ; ; extra/override ZP variables
 ; CURDVC			:= $000E
@@ -34,8 +34,6 @@ USR				:= GORESTART ; XXX
 ; ; constants
 SPACE_FOR_GOSUB := $3E
 STACK_TOP		:= $FA
-CRLF_1          := $20
-CRLF_2          := CR
 WIDTH			:= 40
 WIDTH2			:= 30
 
